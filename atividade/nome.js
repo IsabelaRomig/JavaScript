b62ec246172f8c8b6e1b8f28f85eef84d093ff51
IsabelaRomig <isabela.romig@escola.pr.gov.br>
2025-02-18 08:05:21 -0300

    Delete atividade directory

--- atividade/nome.js
+++
@@ -1,3 +0,0 @@
-const nome = "Isabela";
-
-console.log(nome)
--- atividade/soma.js
+++
@@ -1,6 +0,0 @@
-const primeiroNumero = 5;
-const segundoNumero = 5;
-
-const operacaoMatematica = primeiroNumero + segundoNumero
-
-console.log(operacaoMatematica)
